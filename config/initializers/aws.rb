4a3012ce82209d6374546872c224a8fe221e0123
Michał Siwiec <siwiec.michal724@gmail.com>
2022-02-12 11:11:08 +0100

    ADD configuration for AWS Client

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,7 +3,7 @@ require 'aws-sdk-core'
 Aws.config.update(
   region: 'us-west-2',
   credentials: Aws::Credentials.new(
-    Rails.application.credentials.dig(:aws, :access_key_id)
+    Rails.application.credentials.dig(:aws, :access_key_id),
     Rails.application.credentials.dig(:aws, :secret_access_key)
   )
 )
