ee4ae76c66a2b0d1a5e8ff61f446280eb8bbef7e
Michał Siwiec <siwiec.michal724@gmail.com>
2022-10-12 12:29:31 +0200

    add validation for order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
+  ALLOWED_DELIVERY_METHOD = %w[in_post dpd at_the_point]
+  ALLOWED_PAYMENT_METHOD = %w[traditional_transfer cash_on_delivery]
+  PHONE_NUMBER_REGEX = /\A[0-9]{9}\z/
+  
   belongs_to :user
   has_many :products_orders
+
+  validates :delivery_method, inclusion: { in: ALLOWED_DELIVERY_METHOD }
+  validates :payment_method, inclusion: { in: ALLOWED_PAYMENT_METHOD }
+  validates :phone_number, format: { with: PHONE_NUMBER_REGEX }
 end
