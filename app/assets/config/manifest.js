b0614f8f5b07e30340942de002cffeb8ac6c0fe0
Michał Siwiec <siwiec.michal724@gmail.com>
2022-10-25 11:55:14 +0200

    Fixes to deploy on Heroku

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,4 @@
-//= link graphiql/rails/application.css
-//= link graphiql/rails/application.js
+if (process.env.NODE_ENV === 'development') {
+  //= link graphiql/rails/application.css
+  //= link graphiql/rails/application.js
+}
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,11 +2,8 @@
 
 require 'aws-sdk-core'
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(Rails.application.config.aws_bucket)
-REGION = 'eu-central-1'
-
 Aws.config.update(
-  region: REGION,
+  region: Rails.application.credentials[:AWS_REGION],
   credentials: Aws::Credentials.new(
     Rails.application.credentials.dig(:aws, :access_key_id),
     Rails.application.credentials.dig(:aws, :secret_access_key)
