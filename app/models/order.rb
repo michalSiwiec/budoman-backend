419d80a03f40ebf9cc038a364ae9a14392921423
Michał Siwiec <siwiec.michal724@gmail.com>
2022-10-28 20:22:24 +0200

    add logic to sum delivery to order's total price

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,20 +1,33 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
-  ALLOWED_DELIVERY_METHOD = %w[in_post dpd pick_up_at_the_point].freeze
+  DELIVERIES_DETAILS = [
+    { method: 'in_post', price: 10.99 },
+    { method: 'dpd', price: 15.99 },
+    { method: 'pick_up_at_the_point', price: 0.0 }
+  ].freeze
+
   ALLOWED_PAYMENT_METHOD = %w[cash_payment traditional_transfer].freeze
   PHONE_NUMBER_REGEX = /\A[0-9]{9}\z/.freeze
 
   belongs_to :user
   has_many :products_orders
 
-  validates :delivery_method, inclusion: { in: ALLOWED_DELIVERY_METHOD }
+  validates :delivery_method, inclusion: { in: DELIVERIES_DETAILS.pluck(:method) }
   validates :payment_method, inclusion: { in: ALLOWED_PAYMENT_METHOD }
   validates :phone_number, format: { with: PHONE_NUMBER_REGEX }
 
   def total_price
-    sum = 0
-    products_orders.each { |product_order| sum += product_order.product_quantity * product_order.product.price }
-    sum
+    [price_for_products, price_for_delivery].flatten.sum
+  end
+
+  private
+
+  def price_for_products
+    products_orders.map { |product_order| product_order.product_quantity * product_order.product.price }
+  end
+
+  def price_for_delivery
+    DELIVERIES_DETAILS.find { |delivery_details| delivery_details[:method] == delivery_method }[:price]
   end
 end
