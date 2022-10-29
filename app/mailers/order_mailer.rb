631fb4be24fe047b470cc2714e9ea589c7c45b40
Michał Siwiec <siwiec.michal724@gmail.com>
2022-10-29 15:04:24 +0200

    add invoice to order mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,6 +3,31 @@
 class OrderMailer < ApplicationMailer
   def order_created
     @order = params[:order]
+    @presenter = OrderPresenter.new(@order)
+
+    attach_invoice
+    send_email
+  end
+
+  private
+
+  def attach_invoice
+    attachments['Faktura.pdf'] = invoice_to_attach
+  end
+
+  def invoice_to_attach
+    File.read(URI.parse(url_to_invoice).open)
+  end
+
+  def url_to_invoice
+    "https://#{config.aws_bucket}.#{config.aws_path}/users/#{@order.user.id}/invoices/#{@order.id}.pdf"
+  end
+
+  def config
+    @config ||= Rails.application.config
+  end
+
+  def send_email
     mail(to: @order.email, subject: 'Dziękujemy za zrealizowanie zamówienia!')
   end
 end
