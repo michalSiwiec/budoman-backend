efcb6e1fb50cc36ca8968aaf331f0c9ec9cf50bf
Michal Siwiec <michalsiwiec@MacBook-Pro-Tadeusz.local>
2023-08-21 12:44:06 +0200

    Update mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
+  ACCOUNT_REGISTERED_TITLE = 'Dziękujemy za rejestrację w naszym serwisie!'
+
   def account_registered
     @email = params[:email]
     @password = params[:password]
-    mail(to: @email, subject: 'Dziękujemy za rejestrację w naszym serwisie!')
+    send_email(recipient_email: @email, title: ACCOUNT_REGISTERED_TITLE)
   end
 end
