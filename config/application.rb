0c7091e48ab6fea87762c3e2c582f580a9ac159c
Michal Siwiec <michalsiwiec@MacBook-Pro-Tadeusz.local>
2023-08-27 15:06:49 +0200

    Add configuration for sidekiq panel

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  Sidekiq::Web.use(Rack::Auth::Basic) do |username, password|
+    is_valid_login = ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PANEL_LOGIN']))
+    is_valid_password = ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PANEL_PASSWORD']))
+
+    is_valid_login && is_valid_password
+  end
+
+  mount Sidekiq::Web => '/sidekiq'
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute' if Rails.env.development?
-  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
 
   post '/graphql', to: 'graphql#execute'
 end
