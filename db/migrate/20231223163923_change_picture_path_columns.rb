4247d7f9dce014af298fea057ec44100e3558e1c
Michal Siwiec <michalsiwiec@192.168.1.101>
2024-01-06 16:09:22 +0100

    change default inside products table

--- db/migrate/20231223163923_change_picture_path_columns.rb
+++ db/migrate/20231223163923_change_picture_path_columns.rb
@@ -1,7 +1,9 @@
 class ChangePicturePathColumns < ActiveRecord::Migration[7.1]
   def change
-    add_column :products, :picture_key, :string, limit: 120, null: false
-    add_column :products, :picture_bucket, :string, limit: 120, null: false
+    add_column :products, :picture_key, :string, limit: 120, null: false, default: 'Temporary value'
+    add_column :products, :picture_bucket, :string, limit: 120, null: false, default: 'Temporary value'
     remove_column :products, :picture_path
+    change_column_default :products, :picture_key, from: 'Temporary value', to: nil
+    change_column_default :products, :picture_bucket, from: 'Temporary value', to: nil
   end
 end
