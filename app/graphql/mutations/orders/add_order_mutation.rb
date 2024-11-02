83c41c8ad5489ef110b7ce496b8ab626cef7b65f
Michał Siwiec <michal.siwiec@vouch.us>
2024-11-02 20:17:08 +0100

    fix namespaces

---
+++ app/graphql/mutations/orders/add_order_mutation.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Orders
+    class AddOrderMutation < Mutations::BaseMutation
+      argument :input, Types::Custom::Inputs::Mutations::AddOrderInput, required: true
+      type Types::Custom::Objects::Orders::OrderObject
+  
+      def resolve(params)
+        super(params)
+        ::Orders::HandleAddOrderService.call(params: @params)
+      end
+    end
+  end
+end
